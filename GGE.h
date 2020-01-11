0e38e39b4b8fd675d0c0a47411caf927a5859d1b
Jackson Wambolt <jtwambolt@gmail.com>
2020-01-11 15:46:38 -0600

    Added boilerplate SDL2 code

--- GGE.h
+++ GGE.h
@@ -1,8 +1,50 @@
 #pragma once
 
+#include "SDL.h"
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
-	
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "SDL_Init threw error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
 
+	SDL_Window *window = SDL_CreateWindow("Game", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		std::cout << "SDL_CreateWindow threw error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr)
+	{
+		SDL_DestroyWindow(window);
+		std::cout << "SDL_CreateRenderer threw error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Event event;
+
+	while (1)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			std::cout << "Event: " << event.type << std::endl;
+
+			if (event.type == SDL_QUIT)
+				goto quit;
+		}
+	}
+
+quit:
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	
 	return 0;
 }
