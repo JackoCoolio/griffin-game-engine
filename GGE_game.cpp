2eb7fdcbc80ee55b9038ca9982c932c392702410
Jackson Wambolt <jtwambolt@gmail.com>
2020-01-11 19:27:58 -0600

    Setup framework for game instance

---
+++ GGE_game.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "GGE_game.h"
+
+
+GGE_game::GGE_game()
+{
+}
+
+
+GGE_game::~GGE_game()
+{
+}
+
+void GGE_game::init()
+{
+}
+
+void GGE_game::update(double delta)
+{
+}
+
---
+++ GGE_game.h
@@ -0,0 +1,10 @@
+#pragma once
+class GGE_game
+{
+public:
+	GGE_game();
+	~GGE_game();
+	void init();
+	void update(double delta);
+};
+
