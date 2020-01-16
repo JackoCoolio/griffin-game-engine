076c6ec1fe7e4f636ce06f3bad5c84f4323a9f82
Jackson Wambolt <jtwambolt@gmail.com>
2020-01-15 23:06:25 -0600

    Clean up includes

--- GGE.h
+++ GGE.h
@@ -5,13 +5,7 @@
 
 #include "GGE_event.h"
 #include "GGE_game.h"
-#include "GGE_node.h"
-#include "GGE_world.h"
-#include "GGE_input.h"
 #include "GGE_loop.h"
-#include "GGE_vector.h"
-#include "GGE_camera.h"
-#include "GGE_script.h"
 
 namespace GGE
 {
