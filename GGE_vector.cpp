e61c613a0825791874c655179b87a23ae1013fee
Jackson Wambolt <jtwambolt@gmail.com>
2020-02-21 20:06:29 -0600

    Create operator overloads for Vector2

--- GGE_vector.cpp
+++ GGE_vector.cpp
@@ -1,2 +1,20 @@
 #include "stdafx.h"
 #include "GGE_vector.h"
+
+GGE::Vector2 GGE::Vector2::operator+(const GGE::Vector2 &vec) const
+{
+	Vector2 res{ this->x + vec.x, this->y + vec.y };
+	return res;
+}
+
+GGE::Vector2 GGE::Vector2::operator-(const GGE::Vector2 &vec) const
+{
+	Vector2 res{ this->x - vec.x, this->y - vec.y };
+	return res;
+}
+
+GGE::Vector2 GGE::Vector2::operator*(const float multiplier) const
+{
+	Vector2 res{ this->x * multiplier, this->y * multiplier };
+	return res;
+}
--- GGE_vector.h
+++ GGE_vector.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+
 namespace GGE
 {
 
 	struct Vector2
 	{
 		float x, y;
+		Vector2 operator+(const Vector2&) const;
+		Vector2 operator-(const Vector2&) const;
+		Vector2 operator*(const float) const;
 	};
 
 	struct Vector3
